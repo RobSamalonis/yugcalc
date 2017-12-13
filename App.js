4ab4cd903ec4f9eb0cca45c390d1a6b63df556c6
Rob Samalonis <samalonis@yahoo.com>
2017-12-12 19:32:00 -0500

    Figured out base calculator / react native stuff

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ReactCalculator from './src/ReactCalculator';
+import Style from './Style';
 
 export default class App extends React.Component {
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+      <ReactCalculator />
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
