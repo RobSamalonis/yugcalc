bda42b55c684704913f1648f4e811e7d9b84394f
Rob Samalonis <samalonis@yahoo.com>
2017-12-12 23:38:17 -0500

    Added funcitonality

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import ReactCalculator from './src/ReactCalculator';
-import Style from './Style';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import ReactCalculator from "./src/ReactCalculator";
+import Style from "./Style";
 
 export default class App extends React.Component {
-
   render() {
-    return (
-      <ReactCalculator />
-    )
+    return <ReactCalculator />;
   }
 }
--- Style.js
+++ Style.js
@@ -1,48 +1,48 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 var Style = StyleSheet.create({
-    rootContainer: {
-        flex: 1
-    },
+  rootContainer: {
+    flex: 1
+  },
 
-    displayContainer: {
-        flex: 2,
-        backgroundColor: '#193441',
-        justifyContent: 'center'
-    },
+  displayContainer: {
+    flex: 2,
+    backgroundColor: "#193441",
+    justifyContent: "center"
+  },
 
-    displayText: {
-        color: 'white',
-        fontSize: 38,
-        fontWeight: 'bold',
-        textAlign: 'right',
-        padding: 20
-    },
+  displayText: {
+    color: "white",
+    fontSize: 38,
+    fontWeight: "bold",
+    textAlign: "right",
+    padding: 20
+  },
 
-    inputContainer: {
-        flex: 8,
-        backgroundColor: 'orange'
-    },
-    inputRow: {
-        flex: 1,
-        flexDirection: 'row'
-    },
-    inputButton: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderWidth: '1px',
-        borderColor: '#193441'
-    },
+  inputContainer: {
+    flex: 8,
+    backgroundColor: "orange"
+  },
+  inputRow: {
+    flex: 1,
+    flexDirection: "row"
+  },
+  inputButton: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: "1px",
+    borderColor: "#193441"
+  },
 
-    inputButtonText: {
-        fontSize: 22,
-        fontWeight: 'bold',
-        color: 'white'
-    },
-    inputButtonHighlighted: {
-        backgroundColor: '#193441'
-    }
+  inputButtonText: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "white"
+  },
+  inputButtonHighlighted: {
+    backgroundColor: "#193441"
+  }
 });
 
 export default Style;
